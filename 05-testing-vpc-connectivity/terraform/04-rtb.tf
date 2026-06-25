fa8503d9f57b07e5b629d793c5bc82f38d0992d9
Rami <178672291+RakeemRussell@users.noreply.github.com>
2026-06-25 18:00:50 -0400

    updated 03 and 04 files

---
+++ 05-testing-vpc-connectivity/terraform/04-rtb.tf
@@ -0,0 +1,27 @@
+#public rtb
+resource "aws_route_table" "public-rtb" {
+  vpc_id = aws_vpc.us-east-1
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_us_east_1.id
+  }
+
+  tags = {
+    Name = "public-rtb"
+  }
+}
+
+#private rtb
+resource "aws_route_table" "private-rtb" {
+  vpc_id = aws_vpc.us-east-1
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw_us_east_1.id
+  }
+
+  tags = {
+    Name = "private-rtb"
+  }
+}
